b37b737bb05e46d8e8d4aa073bcaf92df529bfb8
tslxt <15460854@qq.com>
2016-07-27 23:13:02 +0800

    Initial Commit

---
+++ LiveCourse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveCourse
+//
+//  Created by lxt on 16/7/27.
+//  Copyright © 2016年 lxt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
