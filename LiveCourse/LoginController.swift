404fba8fcc08fc9e0f8679f57ad62f096bb141dd
tslxt <15460854@qq.com>
2016-07-29 02:29:28 +0800

    test animation

---
+++ LiveCourse/LoginController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginController.swift
+//  LiveCourse
+//
+//  Created by lxt on 16/7/29.
+//  Copyright © 2016年 lxt. All rights reserved.
+//
+
+import UIKit
+
+class LoginController:UIViewController {
+    
+    override func viewDidLoad() {
+        print("Login controller load...")
+    }
+    
+}
+
